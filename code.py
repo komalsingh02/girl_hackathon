7ac62925d3e66485484c4c9baa40d88e3aa6edb3
komalsingh02 <88960442+komalsingh02@users.noreply.github.com>
2023-07-04 23:49:39 +0530

    1

--- code.py
+++ code.py
@@ -16,11 +16,42 @@ def parse_fault_file(file_path):
                 net, expression = line.strip().split('=')
                 fault[net.strip()] = expression.strip()
     return fault
+def bitwise_eval(expression):
+    operators = {
+        '&': lambda x, y: x & y,
+        '|': lambda x, y: x | y,
+        '^': lambda x, y: x ^ y,
+        '~': lambda x: 0 if x=='1' else 1
+    }
+
+    stack = []
+    expression=expression.split()
+    i=0
+    while i < len(expression):
+        token=expression[i]
+        if token.isdigit():
+            stack.append(int(token))
+            i=i+1
+        elif token in operators:
+            if token == '~':
+                operand = expression[i+1]
+                i=i+2
+                result = operators['~'](operand)
+                stack.append(result)
+            else:
+                operand2 = int(expression[i+1])
+                i=i+2
+                operand1 = int(stack.pop())
+                result = operators[token](operand1, operand2)
+                stack.append(result)
+
+    return stack.pop()
+
 def evaluate_expression(expression, input_vector):
     for variable, value in input_vector.items():
         expression = expression.replace(variable, str(value))
-    return eval(expression)
-
+    ans=bitwise_eval(expression)
+    return ans
 
 def evaluate_correct_output(circuit,input_vector,output_variable):
   expression=circuit[output_variable]
@@ -35,7 +66,6 @@ def evaluate_correct_output(circuit,input_vector,output_variable):
 
   return evaluate_expression(circuit[output_variable],input_vector)
 
-
 def evaluate_wrong_output(circuit,input_vector,output_variable,fault_location,fault_value):
   input=input_vector
   input[fault_location]=fault_value
@@ -48,33 +78,36 @@ def main():
     fault=parse_fault_file("fault.txt")
     fault_location = fault['FAULT_AT']
     fault_type=fault['FAULT_TYPE']
-
+    faul='0'
+    if fault_type.strip() == "SA1":
+        faul='1'
     all_input_combinations = list(itertools.product([0, 1], repeat=4))
 
     test_results = []
-    for input_combination in all_input_combinations:                                            
-        input_vector = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
+    for input_combination in all_input_combinations:
+        input_vector_1 = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
+        input_vector_2={'A': 0, 'B': 0, 'C': 0, 'D': 0}
 
-        input_vector.update(dict(zip(['A', 'B', 'C', 'D'], input_combination)))
-
-        z=evaluate_correct_output(circuit,input_vector,"Z")
+        input_vector_1.update(dict(zip(['A', 'B', 'C', 'D'], input_combination)))
         
-        input_vector.update(dict(zip(['A', 'B', 'C', 'D'], input_combination)))
+        z=evaluate_correct_output(circuit,input_vector_1,"Z")
 
-        z_=evaluate_wrong_output(circuit,input_vector,"Z",fault_location,0)
+        input_vector_2.update(dict(zip(['A', 'B', 'C', 'D'], input_combination)))
 
+        z_=evaluate_wrong_output(circuit,input_vector_2,"Z",fault_location,faul)
+        
         if z!=z_:
-          test_results.append((input_vector,z,z_))
+          test_results.append((input_vector_1,z,z_))
 
-    with open("/content/output.txt", 'w') as output_file:
-        for input_vector, correct_out,faulty_out in test_results:
-            input=[]
-            input.append(input_vector('A'))
-            input.append(input_vector('B'))
-            input.append(input_vector('C'))
-            input.append(input_vector('D'))
-            output_file.write(f"[A,B,C,D]={input},'Z'={faulty_out}\n")
-
+    with open("output.txt", 'w') as output_file:
+       for input_vector, correct_out,faulty_out in test_results:
+          input=[]
+          input.append(input_vector['A'])
+          input.append(input_vector['B'])
+          input.append(input_vector['C'])
+          input.append(input_vector['D'])
+          output_file.write(f"[A,B,C,D]={input},Z={faulty_out}\n")
+          # print(f"[A,B,C,D]={input},Z={faulty_out}\n")
 
 if __name__=='__main__':
   main()
